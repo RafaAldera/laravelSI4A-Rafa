5de6aaa8d1187aa72bb13d46469e462b17689c0d
RafaAldera <iwufkawfe@gmail.com>
2025-06-01 17:52:26 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FakultasController;
 use App\Http\Controllers\ProdiController;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\JadwalController;
+use App\Http\Controllers\MatakuliahController;
+use App\Http\Controllers\SesiController; 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,3 +21,6 @@ Route::resource('/fakultas', FakultasController::class);
 Route::resource('/prodi', ProdiController::class);
 Route::resource('/mahasiswa', MahasiswaController::class);
 Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::resource('/jadwal', JadwalController::class);
+Route::resource('/matakuliah', MatakuliahController::class);
+route::resource('/sesi',SesiController::class);
